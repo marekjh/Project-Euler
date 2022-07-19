933cbb3d033aa565746cf9a6bbf892132bad7b41
marekjh <marekjh1252@gmail.com>
2022-07-19 04:54:47 -0500

    refactored how primes are checked

--- primes.py
+++ primes.py
@@ -1,7 +1,11 @@
+import pickle
+
+with open("primes.pickle", "rb") as f:
+    PRIMES = pickle.load(f)
+
 def generate_primes(N):
-    N **= 2
     primes = [2, 3]
-    for n in range(5, int(N ** 0.5) + 10, 2):
+    for n in range(5, N, 2):
         i = 0
         while primes[i] <= int(n ** 0.5):
             if n % primes[i] == 0:
@@ -11,11 +15,5 @@ def generate_primes(N):
             primes.append(n)
     return primes
 
-def is_prime(n, p):
-    print(n)
-    i = 0
-    while p[i] <= int(n ** 0.5):
-        if n % p[i] == 0:
-            return False
-        i += 1
-    return True
+def is_prime(n):
+    return n in PRIMES
