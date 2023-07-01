6606bc5f9c6aac723f1a1c3a1c40603c1a07e36c
marekjh <marekjh1252@gmail.com>
2023-07-01 04:28:24 -0500

    solved problem 80

---
+++ problems/problem80.py
@@ -0,0 +1,20 @@
+def digits(N, D):
+    S = 0
+    for n in range(2, N+1):
+        if n**0.5 == round(n**0.5):
+            continue
+
+        curr = 0
+        for p in range(D):
+            for d in range(1, 10):
+                new = 10*curr+d
+                if new**2 > n*10**(2*p):
+                    curr = new - 1
+                    break
+            else:
+                curr = new
+        S += sum(int(x) for x in str(curr))
+    
+    return S
+
+print(digits(100, 100))
