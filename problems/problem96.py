11a4ff4c6dd8c8b70addf741adb4f6d3bd06b1ac
marekjh <marekjh1252@gmail.com>
2024-04-11 03:12:56 -0500

    Finished solver

--- problems/problem96.py
+++ problems/problem96.py
@@ -1,53 +1,82 @@
+from copy import copy, deepcopy
+
 N = 9  # Should be a square number
+M = int(N**0.5)
 
 def main():
     puzzles = read_file("../data/p096_sudoku.txt")
     total = 0
+    count = 0
     for puzzle in puzzles:
+        count += 1
+        print(count)
         sol = solve(set_puzzle(puzzle))
-        total += int("".join(sol[0][:3]))
+        total += int(str(list(sol[0][0])[0]) + str(list(sol[0][1])[0]) + str(list(sol[0][2])[0]))
     print(total)
-
 
 def solve(puzzle):
     remaining = set()
     for i in range(N):
         remaining = remaining.union({(i, j) for j in range(N)})
+    return solve_helper(puzzle, remaining)
+
+def solve_helper(puzzle, remaining):
     grid = split_grid()
-    while len(remaining) > 0:
-        for move in (naked_single, hidden_single, locked_candidates):
-            deduction_made = move(puzzle, remaining, grid)
-            if deduction_made is None:
-                return None
-            if deduction_made:
+    while True:
+        for move in (naked_single, hidden_single):
+            deduction = move(puzzle, remaining, grid)
+            if len(remaining) == 0:
+                return puzzle
+            if deduction:
                 break
         else:
             for space in remaining:
                 i, j = space
-                for n in puzzle[i][j]:
+                candidates = puzzle[i][j]
+                for n in candidates:
                     puzzle[i][j] = {n}
-                    sol = solve(puzzle)
+                    try:
+                        sol = solve_helper(deepcopy(puzzle), copy(remaining))
+                    except NoSolution:
+                        sol = None
                     if sol is not None:
                         return sol
-                return None
-    return puzzle
+            return None     # Puzzle has no solution
 
-    
 def update_candidates(puzzle, remove, i, j):
     removed = puzzle[i][j].intersection(remove)
     puzzle[i][j] -= remove
-    if len(puzzle[i][j] == 0):
-        return None
+    if len(puzzle[i][j]) == 0:
+        raise NoSolution
     return len(removed) > 0
 
 def naked_single(puzzle, spaces, grid):
-    pass
+    remove = set()
+    updated = False
+    for i, j in spaces:
+        if len(puzzle[i][j]) == 1:
+            remove.add((i, j))
+            for chunk in (grid["rows"][i] - {(i, j)}, grid["cols"][j] - {(i, j)}, grid["blocks"][i//M][j//M] - {(i, j)}):
+                for x, y in chunk:
+                    check = update_candidates(puzzle, puzzle[i][j], x, y) 
+                    if not updated:
+                        updated = check
+    spaces -= remove
+    return updated
 
 def hidden_single(puzzle, spaces, grid):
-    pass
-
-def locked_candidates(puzzle, spaces, grid):
-    pass
+    updated = False
+    for i, j in spaces:
+        for chunk in (grid["rows"][i] - {(i, j)}, grid["cols"][j] - {(i, j)}, grid["blocks"][i//M][j//M] - {(i, j)}):
+            candidates = set()
+            for x, y in chunk:
+                candidates = candidates.union(puzzle[x][y])
+            candidate = puzzle[i][j] - candidates
+            if len(candidate) == 1:
+                puzzle[i][j] = candidate
+                updated = True
+                break
+    return updated
 
 def set_puzzle(puzzle):
     for i in range(len(puzzle)):
@@ -55,19 +84,19 @@ def set_puzzle(puzzle):
             if e:
                 puzzle[i][j] = {e}
             else:
-                puzzle[i][j] = {x for x in range(N)}
+                puzzle[i][j] = {x for x in range(1, N+1)}
     return puzzle
 
 def split_grid():
     rows = [{(i, j) for j in range(N)} for i in range(N)]
     cols = [{(i, j) for i in range(N)} for j in range(N)]
-    blocks = []; n = int(N**0.5)
-    for i in range(n):
+    blocks = [] 
+    for i in range(M):
         row = []
-        for j in range(n):
+        for j in range(M):
             spaces = set()
-            for k in range(n):
-                spaces = spaces.union({(i*n + k, j*n + l) for l in range(n)})
+            for k in range(M):
+                spaces = spaces.union({(i*M + k, j*M + l) for l in range(M)})
             row.append(spaces)
         blocks.append(row)
     return {"rows": rows, "cols": cols, "blocks": blocks}
@@ -83,10 +112,9 @@ def read_file(file):
         out.append(lines[i:i+9])
     return out
  
+class NoSolution(Exception):
+    pass
+
 if __name__ == "__main__":  
-    puzzles = read_file("../data/p096_sudoku.txt")
-    puzzle = set_puzzle(puzzles[0])
-    print(puzzle[0])
-    print(update_candidates(puzzle, {4, 5, 6}, 0, 2))
-    print(puzzle[0])
+    main()
 
